c8a286b03ec6362afecfc68a2ace15bf134f0bbb
Muhammed Mubashir N <mubashirnanath@gmail.com>
2023-02-04 01:51:28 +0530

    Cors implementation changed

--- dist/server.js
+++ dist/server.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const cors_1 = __importDefault(require("cors"));
 const connection_1 = __importDefault(require("./config/connection"));
 // import routes
 const userRouter_1 = __importDefault(require("./routes/userRouter"));
@@ -15,19 +14,31 @@ const chatRouter_1 = __importDefault(require("./routes/chatRouter"));
 const messageRouter_1 = __importDefault(require("./routes/messageRouter"));
 connection_1.default;
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    origin: ["*"],
-    methods: ["GET", "POST"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Access"],
-}));
+// app.use(
+//   cors(
+//   //   {
+//   //   origin: ["*"],
+//   //   methods: ["GET", "POST","PUT","DELETE","PATCH"],
+//   //   credentials: true,
+//   //   allowedHeaders: ["Content-Type", "Access"],
+//   // }
+//   )
+// );
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://connect.techmart.tech/ , https://www.connect.techmart.tech/');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Pass to next layer of middleware
+    next();
+});
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use("/", userRouter_1.default);
 app.use("/chat/", chatRouter_1.default);
 app.use("/admin", adminRouter_1.default);
 app.use("/message/", messageRouter_1.default);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`server started at port ${port}`);
 });
