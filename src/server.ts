dcb22be3b150aa7bcdff10082c49c0f1729d892d
Muhammed Mubashir N <mubashirnanath@gmail.com>
2023-02-04 01:40:24 +0530

    Cors implementation changed

--- src/server.ts
+++ src/server.ts
@@ -12,16 +12,26 @@ import messageRouter from "./routes/messageRouter";
 
 connectDB;
 const app: Application = express();
-app.use(
-  cors(
-  //   {
-  //   origin: ["*"],
-  //   methods: ["GET", "POST","PUT","DELETE","PATCH"],
-  //   credentials: true,
-  //   allowedHeaders: ["Content-Type", "Access"],
-  // }
-  )
-);
+// app.use(
+//   cors(
+//   //   {
+//   //   origin: ["*"],
+//   //   methods: ["GET", "POST","PUT","DELETE","PATCH"],
+//   //   credentials: true,
+//   //   allowedHeaders: ["Content-Type", "Access"],
+//   // }
+//   )
+// );
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://connect.techmart.tech/ , https://www.connect.techmart.tech/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
