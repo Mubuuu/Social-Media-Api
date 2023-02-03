beb9397470e673034770d5ebc32fdfe1a8a8a7b4
Muhammed Mubashir N <mubashirnanath@gmail.com>
2023-02-04 01:59:28 +0530

    Cors implementation changed

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const app: Application = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.connect.techmart.tech/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.connect.techmart.tech');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
